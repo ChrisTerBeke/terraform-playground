bafe193eea8f0b2b65177038116211ddf4b1411a
Chris ter Beke <christerbeke@binx.io>
2022-01-06 15:13:14 +0100

    Allow SSH access to new VM

--- terraform/applications/gcp-get-started/compute.tf
+++ terraform/applications/gcp-get-started/compute.tf
@@ -2,9 +2,26 @@ resource "google_compute_address" "gcp_get_started_debian_static_ip" {
   name = "gcp-get-started-debian-static-ip"
 }
 
+resource "google_compute_firewall" "gcp_get_started_allow_ssh" {
+  name          = "gcp-get-started-allow-ssh"
+  network       = google_compute_network.gcp_get_started_vpc_network.name
+  target_tags   = ["gcp-get-started-allow-ssh"]
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  depends_on = [
+    google_compute_network.gcp_get_started_vpc_network,
+  ]
+}
+
 resource "google_compute_instance" "gcp_get_started_debian_vm" {
   name         = "gcp-get-started-debian"
   machine_type = "f1-micro"
+  tags         = ["gcp-get-started-allow-ssh"]
 
   boot_disk {
     initialize_params {
