589f6dfd71bcd4c2037acde75322b1d3458d5396
Chris ter Beke <christerbeke@binx.io>
2022-01-06 16:26:40 +0100

    Add some in-line docs on how to connect over SSH

--- terraform/applications/gcp-get-started/outputs.tf
+++ terraform/applications/gcp-get-started/outputs.tf
@@ -10,7 +10,11 @@ output "gcp_get_started_debian_vm_public_ip" {
 output "gcp_get_started_debian_vm_ssh_private_key" {
   description = "The SSH private key to connect to the Debian VM."
   value       = tls_private_key.gcp_get_started_ssh_private_key.private_key_pem
-  sensitive   = true
+
+  // while our private key is sensitive information, we can still obtain it in order to connect to the machine over SSH:
+  // terraform output -json | jq -r ".gcp_get_started_debian_vm_ssh_private_key.value" > ~/.ssh/google_compute_engine
+  // ssh -i ~/.ssh/google_compute_engine christerbeke@35.204.2.125
+  sensitive = true
 
   depends_on = [
     tls_private_key.gcp_get_started_ssh_private_key,
