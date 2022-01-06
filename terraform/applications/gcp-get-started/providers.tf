d4e49eeefcb252f0e6a0eed25d3463a719befb8b
Chris ter Beke <christerbeke@binx.io>
2022-01-06 14:18:50 +0100

    Separate file for services

--- terraform/applications/gcp-get-started/providers.tf
+++ terraform/applications/gcp-get-started/providers.tf
@@ -13,8 +13,3 @@ provider "google" {
   region      = "europe-west4"
   zone        = "europe-west4-a"
 }
-
-resource "google_project_service" "gcp_get_started_project_service" {
-  service            = "compute.googleapis.com"
-  disable_on_destroy = false
-}
