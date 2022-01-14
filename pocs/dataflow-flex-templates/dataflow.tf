4cd77cb208cca49bd0fa43b286a3d89b09513899
Chris ter Beke <christerbeke@binx.io>
2022-01-14 15:37:46 +0100

    Also deploy to dataflow in CI/CI pipeline

--- pocs/dataflow-flex-templates/dataflow.tf
+++ pocs/dataflow-flex-templates/dataflow.tf
@@ -51,6 +51,19 @@ resource "google_storage_bucket_object" "dataflow_metadata" {
   }
 }
 
+resource "google_dataflow_flex_template_job" "dataflow_job" {
+  provider                = google-beta
+  name                    = "dataflow-flex-job"
+  container_spec_gcs_path = "gs://${google_storage_bucket.storage_bucket.name}/${google_storage_bucket_object.dataflow_metadata.name}"
+
+  parameters = {
+    input_subscription = google_pubsub_subscription.pubsub_subscription.id
+    output_table       = "playground-christerbeke:${google_bigquery_dataset.bigquery_dataset.dataset_id}.${google_bigquery_table.bigquery_table.table_id}"
+    subnetwork         = "regions/${google_compute_subnetwork.vpc_subnetwork.region}/subnetworks/${google_compute_subnetwork.vpc_subnetwork.name}"
+    metadata_file_md5  = google_storage_bucket_object.dataflow_metadata.md5hash
+  }
+}
+
 resource "google_cloudbuild_trigger" "cloudbuild_trigger" {
   name           = "dataflow-build"
   included_files = ["pocs/dataflow-flex-templates/template/**"]
@@ -87,18 +100,17 @@ resource "google_cloudbuild_trigger" "cloudbuild_trigger" {
         "--metadata-file", "metadata.json",
       ]
     }
-  }
-}
 
-resource "google_dataflow_flex_template_job" "dataflow_job" {
-  provider                = google-beta
-  name                    = "dataflow-flex-job"
-  container_spec_gcs_path = "gs://${google_storage_bucket.storage_bucket.name}/${google_storage_bucket_object.dataflow_metadata.name}"
-
-  parameters = {
-    input_subscription = google_pubsub_subscription.pubsub_subscription.id
-    output_table       = "playground-christerbeke:${google_bigquery_dataset.bigquery_dataset.dataset_id}.${google_bigquery_table.bigquery_table.table_id}"
-    subnetwork         = "regions/${google_compute_subnetwork.vpc_subnetwork.region}/subnetworks/${google_compute_subnetwork.vpc_subnetwork.name}"
-    metadata_file_md5  = google_storage_bucket_object.dataflow_metadata.md5hash
+    step {
+      id   = "Deploy Dataflow"
+      name = "gcr.io/cloud-builders/gcloud"
+      dir  = "pocs/dataflow-flex-templates"
+      args = [
+        "dataflow", "flex-template", "run", google_dataflow_flex_template_job.dataflow_job.name,
+        "--template-file-gcs-location=gs://${google_storage_bucket.storage_bucket.name}/${google_storage_bucket_object.dataflow_metadata.name}",
+        "--parameters=input_subscription='${google_pubsub_subscription.pubsub_subscription.id}',output_table='playground-christerbeke:${google_bigquery_dataset.bigquery_dataset.dataset_id}.${google_bigquery_table.bigquery_table.table_id}'",
+        "--subnetwork=regions/${google_compute_subnetwork.vpc_subnetwork.region}/subnetworks/${google_compute_subnetwork.vpc_subnetwork.name}"
+      ]
+    }
   }
 }
