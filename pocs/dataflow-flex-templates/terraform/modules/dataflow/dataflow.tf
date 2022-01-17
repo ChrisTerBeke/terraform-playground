598a2c31b156f7b701f1992c3bd0b0c55c5206a7
Chris ter Beke <christerbeke@binx.io>
2022-01-17 11:54:40 +0100

    Add todo

--- pocs/dataflow-flex-templates/terraform/modules/dataflow/dataflow.tf
+++ pocs/dataflow-flex-templates/terraform/modules/dataflow/dataflow.tf
@@ -13,6 +13,7 @@ resource "google_dataflow_flex_template_job" "dataflow_job" {
   container_spec_gcs_path = "gs://${data.google_storage_bucket_object.template_metadata.0.bucket}/${data.google_storage_bucket_object.template_metadata.0.name}"
   on_delete               = "drain"
 
+  // TODO: make parameters configurable and sync with metadata.json
   parameters = {
     input_subscription = google_pubsub_subscription.pubsub_subscription.id
     output_table       = "${data.google_project.current_project.project_id}:${google_bigquery_dataset.bigquery_dataset.dataset_id}.${google_bigquery_table.bigquery_table.table_id}"
