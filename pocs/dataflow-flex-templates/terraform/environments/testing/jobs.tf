522b95e8dd4538be38139bab640e8149f4e0140f
Chris ter Beke <christerbeke@binx.io>
2022-01-17 11:51:14 +0100

    Re-arrange

--- pocs/dataflow-flex-templates/terraform/environments/testing/jobs.tf
+++ pocs/dataflow-flex-templates/terraform/environments/testing/jobs.tf
@@ -1,12 +1,3 @@
-module "deploy_template_simple" {
-  source = "../../modules/deploy"
-
-  name_prefix                = "simple"
-  template_github_repository = "ChrisTerBeke/terraform-playground:main"
-  template_directory         = "pocs/dataflow-flex-templates/templates/simple"
-  storage_location           = local.default_location
-}
-
 module "dataflow_simple" {
   source = "../../modules/dataflow"
 
