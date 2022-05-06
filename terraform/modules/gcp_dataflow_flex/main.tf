c09cddcc6f6a2581385a5740dc1670a5b1caf93b
Chris ter Beke <christerbeke@binx.io>
2022-05-06 13:36:33 +0200

    fix split

--- terraform/modules/gcp_dataflow_flex/main.tf
+++ terraform/modules/gcp_dataflow_flex/main.tf
@@ -1,6 +1,6 @@
 locals {
   dataflow_service_account_roles = concat(["dataflow.worker", "dataflow.admin", "storage.objectViewer"], var.extra_roles)
-  template_bucket                = split(trimprefix(var.template_storage_url, "gs://"), "/")[0]
+  template_bucket                = split("/", trimprefix(var.template_storage_url, "gs://"))[0]
   template_path                  = trimprefix(var.template_storage_url, "gs://${local.template_bucket}")
 }
 
