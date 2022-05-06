f52c113163e8e3ff4f031e9ea21a0482e6de997a
Chris ter Beke <christerbeke@binx.io>
2022-05-06 13:40:08 +0200

    uncomment and fix

--- terraform/modules/gcp_dataflow_flex/main.tf
+++ terraform/modules/gcp_dataflow_flex/main.tf
@@ -1,15 +1,15 @@
 locals {
   dataflow_service_account_roles = concat(["dataflow.worker", "dataflow.admin", "storage.objectViewer"], var.extra_roles)
   template_bucket                = split("/", trimprefix(var.template_storage_url, "gs://"))[0]
-  template_path                  = trimprefix(var.template_storage_url, "gs://${local.template_bucket}")
+  template_path                  = trimprefix(var.template_storage_url, "gs://${local.template_bucket}/")
 }
 
-# data "google_storage_bucket_object" "template_metadata" {
-#   count = var.enabled ? 1 : 0
+data "google_storage_bucket_object" "template_metadata" {
+  count = var.enabled ? 1 : 0
 
-#   name   = local.template_path
-#   bucket = local.template_bucket
-# }
+  name   = local.template_path
+  bucket = local.template_bucket
+}
 
 // TODO: extract service account
 resource "google_service_account" "dataflow_service_account" {
@@ -27,20 +27,20 @@ resource "google_project_iam_member" "dataflow_service_account_iam_member" {
   role    = "roles/${each.key}"
 }
 
-# resource "google_dataflow_flex_template_job" "dataflow_job" {
-#   count = var.enabled ? 1 : 0
+resource "google_dataflow_flex_template_job" "dataflow_job" {
+  count = var.enabled ? 1 : 0
 
-#   provider                = google-beta
-#   project                 = var.project_id
-#   name                    = "${var.name_prefix}-job"
-#   region                  = var.region
-#   container_spec_gcs_path = "gs://${data.google_storage_bucket_object.template_metadata[0].bucket}/${data.google_storage_bucket_object.template_metadata[0].name}"
-#   on_delete               = "drain"
+  provider                = google-beta
+  project                 = var.project_id
+  name                    = "${var.name_prefix}-job"
+  region                  = var.region
+  container_spec_gcs_path = "gs://${data.google_storage_bucket_object.template_metadata[0].bucket}/${data.google_storage_bucket_object.template_metadata[0].name}"
+  on_delete               = "drain"
 
-#   parameters = merge({
-#     subnetwork            = var.vcp_subnet_name
-#     service_account_email = google_service_account.dataflow_service_account[0].email
-#     max_num_workers       = var.max_workers
-#     metadata_file_md5     = data.google_storage_bucket_object.template_metadata[0].md5hash // triggers re-deployment when template is updated via Cloud Build
-#   }, var.job_parameters)
-# }
+  parameters = merge({
+    subnetwork            = var.vcp_subnet_name
+    service_account_email = google_service_account.dataflow_service_account[0].email
+    max_num_workers       = var.max_workers
+    metadata_file_md5     = data.google_storage_bucket_object.template_metadata[0].md5hash // triggers re-deployment when template is updated via Cloud Build
+  }, var.job_parameters)
+}
