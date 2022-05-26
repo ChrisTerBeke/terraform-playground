d5df1f1d6ad7459c92c56800a63042f7168949e3
Chris ter Beke <christerbeke@binx.io>
2022-05-26 13:56:05 +0200

    Test

--- terraform/modules/gcp_serverless_ingress/proxy.tf
+++ terraform/modules/gcp_serverless_ingress/proxy.tf
@@ -5,7 +5,7 @@ resource "google_compute_target_https_proxy" "https_proxy" {
   name             = "${var.name}-target-proxy"
   url_map          = google_compute_url_map.url_map.0.id
   ssl_policy       = google_compute_ssl_policy.ssl_policy.0.id
-  ssl_certificates = [] // google_compute_managed_ssl_certificate.ssl_cert.0.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.ssl_cert.0.id]
 }
 
 resource "google_compute_target_http_proxy" "http_proxy" {
