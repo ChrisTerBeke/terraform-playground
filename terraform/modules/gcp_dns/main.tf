5f87610cd2cf9266d61fae2ee95fe70fcea9136b
Chris ter Beke <christerbeke@binx.io>
2022-06-08 13:14:21 +0200

    Fix record name

--- terraform/modules/gcp_dns/main.tf
+++ terraform/modules/gcp_dns/main.tf
@@ -10,7 +10,7 @@ resource "google_dns_record_set" "a_records" {
 
   project      = var.project_id
   managed_zone = google_dns_managed_zone.zone.name
-  name         = "${each.key}.${var.domain}"
+  name         = "${each.key}.${google_dns_managed_zone.zone.dns_name}"
   type         = "A"
   ttl          = 60
   rrdatas      = each.value
