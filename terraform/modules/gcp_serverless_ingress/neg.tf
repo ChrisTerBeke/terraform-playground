835065cec9f1243f502f541c0d31313916a12dea
Chris ter Beke <christerbeke@binx.io>
2022-07-06 08:38:28 +0200

    Remove old NEG

--- terraform/modules/gcp_serverless_ingress/neg.tf
+++ terraform/modules/gcp_serverless_ingress/neg.tf
@@ -1,17 +1,3 @@
-resource "google_compute_region_network_endpoint_group" "neg" {
-  for_each = var.cloud_run_services
-
-  project               = var.project_id
-  name                  = "${var.name}-neg"
-  region                = each.key
-  network_endpoint_type = "SERVERLESS"
-
-  // TODO: make dynamic based on serverless type?
-  cloud_run {
-    service = each.value
-  }
-}
-
 resource "google_compute_region_network_endpoint_group" "network_endpoint_group" {
   for_each = var.cloud_run_services
 
